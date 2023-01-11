b3cbe6e8f0167b8f8882de782537a6f26df56ca3
Vu Long Vu <vuvl@luciscity.io>
2023-01-11 18:12:13 +0700

    vuvl fix withdraw amount

--- apps/lc-be/src/blockchain/transaction.service.ts
+++ apps/lc-be/src/blockchain/transaction.service.ts
@@ -66,16 +66,15 @@ export class TransactionService {
       this.logger.error('Out of money');
       throw new AppError('Balance pool not enough!', 'BALANCE_POOL_NOT_ENOUGH');
     }
+    await this.prismaService.wallet.update({
+      where: { user_id: userId },
+      data: { balance: wallet.balance.minus(amount) },
+    });
 
     const prk = await decrypt(poolWallet.prv);
     const hash = await erc20Service.transfer(address, ethers.utils.parseUnits(amount).toString(), prk);
     // add transaction hash to db
     const response = await this.prismaService.$transaction(async (tx) => {
-      await tx.wallet.update({
-        where: { user_id: userId },
-        data: { balance: wallet.balance.minus(amount) },
-      });
-
       return await tx.transactionLog.create({
         data: {
           type: 'WITHDRAW_BALANCE',
